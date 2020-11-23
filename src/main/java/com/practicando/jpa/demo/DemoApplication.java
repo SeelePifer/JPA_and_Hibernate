5bcc10ef571aa2ed38281803444028107470f602
SeelePifer <lftaddey@gmail.com>
2020-11-23 10:33:45 -0500

    'actualizacion'

--- src/main/java/com/practicando/jpa/demo/DemoApplication.java
+++ src/main/java/com/practicando/jpa/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.practicando.jpa.demo;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,9 +12,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.practicando.jpa.demo.entity.Course;
+import com.practicando.jpa.demo.entity.FullTimeEmployee;
+import com.practicando.jpa.demo.entity.PartTimeEmployee;
 import com.practicando.jpa.demo.entity.Review;
 import com.practicando.jpa.demo.entity.Student;
 import com.practicando.jpa.demo.repository.CourseRepository;
+import com.practicando.jpa.demo.repository.EmployeeRepository;
 import com.practicando.jpa.demo.repository.StudentRepository;
 
 @SpringBootApplication
@@ -24,6 +28,8 @@ public class DemoApplication implements CommandLineRunner {
 	private CourseRepository courseRepository;
 	@Autowired
 	private StudentRepository srepository;
+	@Autowired
+	private EmployeeRepository erepository;
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -37,8 +43,15 @@ public class DemoApplication implements CommandLineRunner {
 	    reviews.add(new Review("5", "Great Hands-on Stuff."));  
 	    reviews.add(new Review("5", "Hatsoff."));
 	    courseRepository.addReviewsForCourse(10003L, reviews ); */
-		srepository.insertStudentAndCourse(new Student("Jack"),
-				new Course("Microservices in 100 Steps"));
+		/*srepository.insertStudentAndCourse(new Student("Jack"),
+				new Course("Microservices in 100 Steps"));*/
+		
+		erepository.insertEmployee(new FullTimeEmployee("Jack",
+				new BigDecimal("10000")));
+		erepository.insertEmployee(new PartTimeEmployee("Jill",
+				new BigDecimal("50")));
+		logger.info("All Employees. ->{}"
+				,erepository.retrieveAllEmployee());
 	}
 
 }
